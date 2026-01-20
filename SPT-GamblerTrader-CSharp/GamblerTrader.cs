fb010c2a55899e6b0deb2dd117dbac472b4a267b
Victor <hoodrich10135@gmail.com>
2026-01-20 01:09:34 -0800

    init

---
+++ SPT-GamblerTrader-CSharp/GamblerTrader.cs
@@ -0,0 +1,63 @@
+﻿using System.Reflection;
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.Controllers;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Common;
+using SPTarkov.Server.Core.Models.Eft.Common;
+using SPTarkov.Server.Core.Models.Eft.Inventory;
+using SPTarkov.Server.Core.Models.Eft.ItemEvent;
+using SPTarkov.Server.Core.Models.Utils;
+using SPTarkov.Server.Core.Servers;
+using SPTarkov.Reflection.Patching;
+using SPTarkov.Server.Core.Utils;
+
+namespace SPT_GamblerTrader_CSharp;
+
+
+[Injectable(TypePriority = OnLoadOrder.PreSptModLoader)]
+public class OpenRandomLootContainerPatch : AbstractPatch
+{
+    protected override MethodBase? GetTargetMethod()
+    {
+        var inventoryController = typeof(InventoryController);
+        return inventoryController.GetMethod(
+            "OpenRandomLootContainer",
+            [
+                typeof(PmcData),
+                typeof(OpenRandomLootContainerRequestData),
+                typeof(MongoId),
+                typeof(ItemEventRouterResponse)
+            ]
+        );
+    }
+
+    [PatchPrefix]
+    public static bool Prefix(InventoryController __instance,
+                              PmcData pmcData,
+                              OpenRandomLootContainerRequestData request,
+                              MongoId sessionId,
+                              ItemEventRouterResponse output)
+    {
+        ServiceLocator.ServiceProvider.GetService<ISptLogger<App>>().Success("OpenRandomLootContainer intercepted by GamblerTrader");
+
+        // Prevent original method from running 
+        return false;
+    }
+}
+
+
+[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 1)]
+public class GamblerTrader(
+    ISptLogger<GamblerTrader> logger,
+    ConfigServer configServer
+    ) : IOnLoad
+{
+    public Task OnLoad()
+    {
+
+        new OpenRandomLootContainerPatch().Enable();
+
+        logger.Info("Gambler Trader Loaded Successfully!");
+        return Task.CompletedTask;
+    }
+}
---
+++ SPT-GamblerTrader-CSharp/ModMetadata.cs
@@ -0,0 +1,18 @@
+
+using SPTarkov.Server.Core.Models.Spt.Mod;
+
+namespace HoodsEnergyDrinks_CSharp;
+public record ModMetadata : AbstractModMetadata
+{
+    public override string Name { get; init; } = "Gambler Trader";
+    public override string Author { get; init; } = "Hood";
+    public override List<string>? Contributors { get; init; }
+    public override SemanticVersioning.Version Version { get; init; } = new("1.0.0");
+    public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
+    public override List<string>? Incompatibilities { get; init; }
+    public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
+    public override string? Url { get; init; } = "https://github.com/Hood26/HoodsEnergyDrinks-CSharp/tree/master";
+    public override bool? IsBundleMod { get; init; } = true;
+    public override string? License { get; init; } = "MIT";
+    public override string ModGuid { get; init; } = "com.hood.gamblertrader";
+}
